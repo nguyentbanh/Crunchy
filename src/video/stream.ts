cbafa5bc903f438207c71da9d633a656e7bfa729
Godzil <godzil@godzil.net>
2018-06-11 21:35:17 +0100

    Now that retrying is there; force ffmpeg to fail in case or error (should now avoid to silently download a corrupted video file)

--- src/video/stream.ts
+++ src/video/stream.ts
@@ -24,7 +24,7 @@ export default function(rtmpUrl: string, rtmpInputPath: string, swfUrl: string, 
   else if (mode === 'HLS')
   {
       cmd = command('ffmpeg') + ' ' +
-          '-y ' +
+          '-y -xerror ' +
           '-i "' + rtmpInputPath + '" ' +
           '-c copy -bsf:a aac_adtstoasc ' +
           '"' + filePath + '.mp4"';
